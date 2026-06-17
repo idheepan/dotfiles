7c5271cc91102c0da2cb82707d4c0dc3316bf8ff
idheepan <dhpn@mac.local>
2026-06-17 14:32:12 -0400

    feat: add .local/bin to path in zshrc, modified completion.lua to use llama server.

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,10 @@
 return {
-  "stevearc/conform.nvim",
-  optional = true,
-  opts = {
-    formatters_by_ft = {
-      python = { "black", "ruff", "ruff_organize_imports" },
-      markdown = {
-        "markdownlint-cli2",
-      },
-    },
-  },
+	"stevearc/conform.nvim",
+	optional = true,
+	opts = {
+		formatters_by_ft = {
+			python = { "black", "ruff", "ruff_organize_imports" },
+			markdown = { "prettier" },
+		},
+	},
 }
--- private_dot_config/nvim/lua/plugins/nvim-lint.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -1,20 +1,15 @@
 return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      markdown = "markdownlint-cli2",
-    },
-    linters = {
-      ["markdownlint-cli2"] = {
-        stdin = false,
-        args = {
-          ".",
-        },
-        parser = require("lint.parser").from_errorformat(
-          "%f:%l:%c %m,%f:%l %m",
-          { source = "markdownlint", severity = vim.diagnostic.severity.WARN }
-        ),
-      },
-    },
-  },
+	"mfussenegger/nvim-lint",
+	opts = {
+		linters = {
+			["markdownlint-cli2"] = {
+				stdin = false,
+				args = {},
+				parser = require("lint.parser").from_errorformat(
+					"%f:%l:%c %m,%f:%l %m",
+					{ source = "markdownlint", severity = vim.diagnostic.severity.WARN }
+				),
+			},
+		},
+	},
 }
