6d67a062f250655df8df87b279163dc47be918da
idheepan <dhpn@mac.local>
2026-06-17 18:47:37 -0400

    fix: remove extra_args --verbose for typst preview in nvim.

--- private_dot_config/nvim/lua/plugins/typst-preview.lua
+++ private_dot_config/nvim/lua/plugins/typst-preview.lua
@@ -1,23 +1,22 @@
 return {
-  "chomosuke/typst-preview.nvim",
-  lazy = false, -- or ft = 'typst'
-  version = "1.*",
-  opts = {
-    open_cmd = "open %s",
-    dependencies_bin = {
-      ["tinymist"] = vim.fn.exepath("tinymist"),
-      ["websocat"] = vim.fn.exepath("websocat"),
-    },
-    extra_args = { "--verbose" },
-  },
-  keys = {
-    {
-      "<leader>cp",
-      function()
-        vim.cmd.TypstPreviewToggle()
-      end,
-      desc = "Typst Preview Toggle",
-      ft = "typst", -- Only available in typst files
-    },
-  },
+	"chomosuke/typst-preview.nvim",
+	lazy = false, -- or ft = 'typst'
+	version = "1.*",
+	opts = {
+		open_cmd = "open %s",
+		dependencies_bin = {
+			["tinymist"] = vim.fn.exepath("tinymist"),
+			["websocat"] = vim.fn.exepath("websocat"),
+		},
+	},
+	keys = {
+		{
+			"<leader>cp",
+			function()
+				vim.cmd.TypstPreviewToggle()
+			end,
+			desc = "Typst Preview Toggle",
+			ft = "typst", -- Only available in typst files
+		},
+	},
 }
