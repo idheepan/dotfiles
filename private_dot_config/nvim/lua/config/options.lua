5a764008690f808ce152127f7a27ad28eb412511
idheepan <idheepan@outlook.com>
2026-07-13 13:49:13 -0400

    feat: disable autoformat on save

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -3,6 +3,9 @@
 -- Add any additional options here
 vim.g.snacks_animate = false
 
+-- Disable format-on-save; format manually with <leader>cf (or <leader>uf to toggle autoformat back on)
+vim.g.autoformat = false
+
 vim.o.clipboard = "unnamedplus"
 
 local function paste()
